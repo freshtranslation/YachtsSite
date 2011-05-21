3ea6c5d756b45c35c9a7884254ef89c1a223d358
Fresh Translation <katty@freshtranslation.com>
2011-05-21 15:12:22 -0300

    enforce our site index page fix jQuery reference in essentialjs

--- js/essentialjs.js
+++ js/essentialjs.js
@@ -1,7 +1,7 @@
 (function(){
     
 if (/ no-horizontal-scroll/.test(document.documentElement.className)) {
-    $(window).bind("scroll",function(event){
+    jQuery(window).bind("scroll",function(event){
         console.log(document.documentElement.scrollTop+" "+document.documentElement.scrollLeft);
         document.documentElement.scrollLeft = 0; // no horizontal scroll
     });
