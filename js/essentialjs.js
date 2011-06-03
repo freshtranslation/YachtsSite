e5f9549af9c2fe9f002868378bc1c2e412241ae4
Fresh Translation <katty@freshtranslation.com>
2011-06-02 21:54:44 -0300

    removed console log

--- js/essentialjs.js
+++ js/essentialjs.js
@@ -2,7 +2,6 @@
     
 if (/ no-horizontal-scroll/.test(document.documentElement.className)) {
     jQuery(window).bind("scroll",function(event){
-        console.log(document.documentElement.scrollTop+" "+document.documentElement.scrollLeft);
         document.documentElement.scrollLeft = 0; // no horizontal scroll
     });
 }
