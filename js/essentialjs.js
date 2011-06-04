3abb5bb2a25811608db9d9ef35c6c733dd2a741a
Fresh Translation <katty@freshtranslation.com>
2011-06-04 19:18:37 -0300

    mail-encode for space replacement in mailto: forms

--- js/essentialjs.js
+++ js/essentialjs.js
@@ -6,4 +6,10 @@ if (/ no-horizontal-scroll/.test(document.documentElement.className)) {
     });
 }
 
+jQuery("textarea.mail-encode").each(function(){
+    this.innerHTML = this.innerHTML.replace(/ /g,"\u00a0");
+});
+jQuery("input.mail-encode").each(function(){
+    this.value = this.value.replace(/ /g,"\u00a0");
+});
 })();
